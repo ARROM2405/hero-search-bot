eadf96f920289cf5ec969cf09846d874d2a29ce9
A R <ar@As-MacBook-Pro.local>
2024-05-08 14:22:28 +0200

    Moved SECRET_KEY from settings to the .env file.

--- hero_search_bot/settings.py
+++ hero_search_bot/settings.py
@@ -15,15 +15,18 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i)b#2$m*jv%%h$dw1)6$aqk4a!t^n$c^wqc(316uwy$eb)%0uq"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +81,6 @@ WSGI_APPLICATION = "hero_search_bot.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
-load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 DATABASES = {
     "default": {
